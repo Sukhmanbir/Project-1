1201e6d6bb446979088c3926c927675f2119a8fa
naes <naes@NAES-PC>
2016-06-21 16:41:02 -0400

    Added OwinStartup class to fix breaking build

---
+++ Project-1/OwinStartup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Project_1.OwinStartup))]
+
+namespace Project_1
+{
+    public class OwinStartup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+        }
+    }
+}
