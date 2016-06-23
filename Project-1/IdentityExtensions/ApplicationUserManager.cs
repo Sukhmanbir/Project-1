d0523d8aff973a10b48d48d9b9871538bc38a042
Sukhmanbir Kaur <sukhmanbir.kaur@outlook.com>
2016-06-23 00:35:43 -0400

    Added ApplicationUserManager class

---
+++ Project-1/IdentityExtensions/ApplicationUserManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Project_1.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Project_1.IdentityExtensions
+{
+    class ApplicationUserManager : UserManager<IdentityUser>
+    {
+        public ApplicationUserManager() : base(new UserStore<IdentityUser>(new IdentityDbContext()))
+         {
+               PasswordValidator = new MinimumLengthValidator(10);
+         }
+}
+}
